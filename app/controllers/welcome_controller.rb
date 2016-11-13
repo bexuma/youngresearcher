8fafb1787372c5215aa6cba5d7abf42262749659
Bexultan Myrzatay <bexultan.dev@gmail.com>
2016-11-13 16:47:59 +0900

    Styling the welcome page part 1

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,6 @@
 class WelcomeController < ApplicationController
+	def index
+		@clarifies = Clarify.all.limit(3).order("created_at desc")
+		@questions = Question.all.limit(3).order("created_at desc")
+	end
 end
